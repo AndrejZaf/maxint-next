9dc5c53c93f5effaa4925c7317fd32e2dc24e660
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-02-10 15:18:33 +0100

    Add scrollbar to the dynamic tabs

--- components/explore/explore-tabs.tsx
+++ components/explore/explore-tabs.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AvailableCategory } from "@/types/available-category";
 import { CircleDollarSign } from "lucide-react";
@@ -6,16 +7,21 @@ import React from "react";
 const ExploreTabs = ({ tabs }: { tabs: AvailableCategory[] }) => {
     return (
         <Tabs defaultValue="checking">
-            <TabsList>
-                {tabs.sort((a, b) => a.offerCategory.localeCompare(b.offerCategory)).map(category => {
-                    return <TabsTrigger value={category.offerCategory} key={category.offerCategory}>
-                        <div className="flex items-center gap-2">
-                            <CircleDollarSign size={16} />
-                            {category.offerCategory}
-                        </div>
-                    </TabsTrigger>
-                })}
-            </TabsList>
+            <ScrollArea>
+                <div className="w-full relative h-10">
+                    <TabsList className="flex absolute h-10">
+                        {tabs.sort((a, b) => a.offerCategory.localeCompare(b.offerCategory)).map(category => {
+                            return <TabsTrigger value={category.offerCategory} key={category.offerCategory}>
+                                <div className="flex items-center gap-2">
+                                    <CircleDollarSign size={16} />
+                                    {category.offerCategory}
+                                </div>
+                            </TabsTrigger>;
+                        })}
+                    </TabsList>
+                </div>
+                <ScrollBar orientation={"horizontal"} />
+            </ScrollArea>
             <TabsContent value="deposit">Make changes to your account here.</TabsContent>
             <TabsContent value="credit">Change your password here.</TabsContent>
         </Tabs>
