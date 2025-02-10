43a731d7ef230e2af8772e79cfa3175cded3fcfc
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-02-10 09:30:45 +0100

    Create base login screen

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,16 @@
+import { LoginForm } from "@/components/login-form";
 import React from "react";
 
 const SignInPage = () => {
     return (
-        <div>SignInPage</div>
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <span className=" text-center text-3xl truncate font-['BC_Alphapipe_TSB_Bold']">
+                  Maxint
+                </span>
+                <LoginForm />
+            </div>
+        </div>
     );
 };
 export default SignInPage;
